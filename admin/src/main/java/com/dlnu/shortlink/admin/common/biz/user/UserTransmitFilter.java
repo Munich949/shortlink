9df2599f1cc81827a917d7227cab0a1882cdda78
Munich <2493929776@qq.com>
2024-07-09 23:33:50 +0800

    feature: 封装用户上下文

---
+++ admin/src/main/java/com/dlnu/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dlnu.shortlink.admin.common.biz.user;
+
+import com.alibaba.fastjson2.JSON;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Objects;
+
+/**
+ * 用户信息传输过滤器
+ */
+@RequiredArgsConstructor
+public class UserTransmitFilter implements Filter {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @SneakyThrows
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!Objects.equals(requestURI, "/api/short-link/admin/v1/user/login")) {
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
+            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+            if (userInfoJsonStr != null) {
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
+        }
+    }
+}
