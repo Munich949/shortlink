f98b6b187400f9d147f8bce9bb3d5860dda9c52b
Munich <2493929776@qq.com>
2024-07-21 16:30:17 +0800

    feature: 开发短链接项目新增短链接功能

---
+++ project/src/main/java/com/dlnu/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dlnu.shortlink.project.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dlnu.shortlink.project.dao.entity.ShortLinkDO;
+import com.dlnu.shortlink.project.dao.mapper.ShortLinkMapper;
+import com.dlnu.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.dlnu.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.dlnu.shortlink.project.service.ShortLinkService;
+import com.dlnu.shortlink.project.toolkit.HashUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 短链接接口实现层
+ */
+@Slf4j
+@Service
+public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
+
+    @Override
+    public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String shortLinkSuffix = generateSuffix(requestParam);
+        ShortLinkDO shortLinkDO = BeanUtil.toBean(requestParam, ShortLinkDO.class);
+        shortLinkDO.setShortUri(shortLinkSuffix);
+        shortLinkDO.setEnableStatus(0);
+        shortLinkDO.setFullShortUrl(requestParam.getDomain() + "/" + shortLinkSuffix);
+        baseMapper.insert(shortLinkDO);
+        return ShortLinkCreateRespDTO.builder()
+                .fullShortUrl(shortLinkDO.getFullShortUrl())
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .build();
+    }
+
+    private String generateSuffix(ShortLinkCreateReqDTO requestParam) {
+        String originUrl = requestParam.getOriginUrl();
+        return HashUtil.hashToBase62(originUrl);
+    }
+}
