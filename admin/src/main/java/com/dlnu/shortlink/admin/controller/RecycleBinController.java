3a7c156802bf78612469bdd1a6aeb7895f6155b6
Munich <2493929776@qq.com>
2024-12-17 23:52:12 +0800

    feature: 开发短链接保存回收站功能

---
+++ admin/src/main/java/com/dlnu/shortlink/admin/controller/RecycleBinController.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dlnu.shortlink.admin.controller;
+
+import com.dlnu.shortlink.admin.common.result.Result;
+import com.dlnu.shortlink.admin.common.result.Results;
+import com.dlnu.shortlink.admin.remote.ShortLinkRemoteService;
+import com.dlnu.shortlink.admin.remote.dto.req.RecycleBinSaveReqDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 回收站管理控制层
+ */
+@RestController(value = "recycleBinControllerByAdmin")
+@RequiredArgsConstructor
+public class RecycleBinController {
+
+    /**
+     * 后续重构为 SpringCloud Feign 调用
+     */
+    ShortLinkRemoteService shortLinkRemoteService = new ShortLinkRemoteService() {
+    };
+
+    /**
+     * 保存回收站
+     */
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/save")
+    public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
+        shortLinkRemoteService.saveRecycleBin(requestParam);
+        return Results.success();
+    }
+}
